3ae80415198f3e5b30666d24c57e05b136e09fe3
RegulusReggie <wrhuang@ucla.edu>
2017-11-25 00:33:28 -0800

    add --help to feature_vector_generator.py

--- feature_vector_generator.py
+++ feature_vector_generator.py
@@ -132,7 +132,7 @@ if __name__ == '__main__':
     test_out_file = ''
     test_label_file = 'test_labels'
     
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 1 or sys.argv[1] == '--help':
         printHelp()
         sys.exit()
     else:
